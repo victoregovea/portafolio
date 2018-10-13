a3a6cb600fbbe4f5c356b917d098e0f42385eb7a
victoregovea <victoregovea@gmail.com>
2018-10-13 01:17:45 -0400

    2 section done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var options = {
+    strings: ["HTML5", "CSS3", "JavaScript", "jQuery", "Bootstrap", "Angular", "React", "WordPress", "SEO"],
+    typeSpeed: 200,
+    loop: true,
+    backSpeed: 100,
+    backDelay: 1000,
+    startDelay: 1000,
+  }
+  
+  var typed = new Typed(".typed", options);
